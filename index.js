4c13f8e37a1d6a56f9d5e1222b111bf05f045580
Gael Mayer <gaelmayer@yahoo.fr>
2021-06-07 16:44:27 +0200

    Element with DOM init in js file

---
+++ index.js
@@ -0,0 +1,13 @@
+let newGame = document.getElementById('new-game');
+
+let playerOne = document.getElementById('player-one');
+let scorePlayerOne = document.getElementById('score-left');
+let temporaryScorePlayerOne = document.getElementById('tp-score-l');
+
+let playerTwo = document.getElementById('player-two');
+let scorePlayerTwo = document.getElementById('score-right');
+let temporaryScorePlayerTwo = document.getElementById('tp-score-r');
+
+
+let rollDice = document.getElementById('roll-dice');
+let hold = document.getElementById('hold');
