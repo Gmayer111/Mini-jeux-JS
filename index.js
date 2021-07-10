835ecc25cf0d06d388fd8584c3e764ea8a90d0a3
Gael Mayer <gaelmayer@yahoo.fr>
2021-07-10 23:03:56 +0200

    finalisation script js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ let scorePlayerOne = document.getElementById('scoreLeft');
 let scorePlayerTwo = document.getElementById('score-right');
 let temporaryScorePlayerOne = document.getElementById('temp-scoreleft');
 let temporaryScorePlayerTwo = document.getElementById('temp-scoreright');
-let rollDice = document.getElementById('roll-dice');
 let one = document.getElementById('one');
 let two = document.getElementById('two');
 let three = document.getElementById('three');
@@ -13,55 +12,47 @@ let five = document.getElementById('five');
 let six = document.getElementById('six');
 let ngBtn = document.getElementById('new-game')
 
+let clickCounter = 0;
 
-ngBtn.addEventListener('click', init)
-
-function init() {
+ngBtn.addEventListener('click', () => {
     
-    let player = Math.floor(Math.random() * (3 - 1) + 1); 
-
     scorePlayerOne.innerHTML = "00";
     scorePlayerTwo.innerHTML = "00";
     temporaryScorePlayerOne.innerHTML = "00";
     temporaryScorePlayerTwo.innerHTML = "00";  
-    iniScore = 0;
+    iniScoreL = 0;
+    iniScoreR = 0;
     one.style = "";
     two.style = "";
     three.style = "";
     four.style = "";
     five.style = "";
     six.style = "";
-    playerOne.style = "";
-    playerTwo.style = "";
 
-    if (player === 1) {
-        playerTwo.style.display = "none";
-        return player;
-    }else if (player === 2) {
-        playerOne.style.display = "none";
-        return player;
-    }else {
-        alert(`Une erreur est survenue`);
-    }
-
-};
-
-let clickCounter = 0;
-let iniScore = 0;
-
-rollDice.addEventListener('click', rollingDice)
-function rollingDice() {
-
+    
+    let player = Math.floor(Math.random() * (3 - 1) + 1); 
     clickCounter++
 
+    if (clickCounter === 1) {
+        playerOne.style.display = "";
+        playerTwo.style.display = "none";
+    }
+    
+    if (player === 1 && clickCounter > 1) {
+        playerOne.style.display = "";
+        playerTwo.style.display = "none";
+    }else if (player === 2 && clickCounter > 1) {
+        playerOne.style.display = "none";
+        playerTwo.style.display = "";
+    }
+
+});
+
+document.getElementById('roll-dice').addEventListener('click', rollingDice)
+
+function rollingDice() {
+
     let result = Math.floor(Math.random() * (7 - 1) + 1);
-    iniScore += result;
-
-    temporaryScorePlayerOne.innerHTML = iniScore;
-    temporaryScorePlayerTwo.innerHTML = iniScore;
-    playerOne.style.display = "none"
-    playerTwo.style.display = ""
-
 
     one.style = "";
     two.style = "";
@@ -113,22 +104,80 @@ function rollingDice() {
         default:
             alert(`Une erreur est survenue`);
             break;
+    } 
+
+    iniScoreL += result;
+    iniScoreR += result;
+
+    if (playerTwo.style.display === "none" ) {
+        playeOne(iniScoreL)
+    } else {
+        playeTwo(iniScoreR)
     }    
-
-    return result;
-
     
+    function playeOne(iniScoreL) {
+   
+        iniScoreR = 0;
+        if (result !== 1) {
+            temporaryScorePlayerOne.innerHTML = iniScoreL; 
+        }else {
+            playerOne.style.display = "none" 
+            playerTwo.style.display = "" 
+            result = 0;
+            iniScoreL = 0;
+            playeTwo(iniScoreR)
+        }
+    }
+
+    function playeTwo(iniScoreR) {
+        iniScoreL = 0;
+        if (result !== 1){
+            temporaryScorePlayerTwo.innerHTML = iniScoreR; 
+        }else {
+            playerOne.style.display = "" 
+            playerTwo.style.display = "none" 
+            result = 0;
+            iniScoreR += result = 0;
+            playeOne(iniScoreL)
+        }
+    }
 }
 
+
+let a = 0;
+let b = 0;
+let clicked = 0;
 //Fonction hold btn
 document.getElementById('hold').addEventListener('click', () => {
 
-    let score = rollingDice();
+    clicked++
 
-    if (temporaryScorePlayerOne) {
-        document.getElementById('scoreLeft').innerHTML = score
+let x = Number(temporaryScorePlayerOne.innerHTML)
+let j = Number(temporaryScorePlayerTwo.innerHTML)
+a += x;
+
+console.log(`x : ${x}`)
+console.log(`a : ${a}`)
+
+b += j
+console.log(`j : ${j}`)
+
+    if (playerTwo.style.display === "none") {
+            scorePlayerOne.textContent = a;
+            playerOne.style.display = "none"
+            playerTwo.style.display = ""
     }else {
-        document.getElementById('score-right').innerHTML = score
+        scorePlayerTwo.innerHTML = b;
+        playerTwo.style.display = "none"
+        playerOne.style.display = ""
+    }
+
+    if (a >= 100) {
+        scorePlayerOne.textContent = "Winner"
+    }
+
+    if (b >= 100) {
+        scorePlayerTwo.textContent = "Winner"
     }
 })
 
